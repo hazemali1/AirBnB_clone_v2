c4b16e36d974ac3bb537a597f48328f6d550cdcd
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-19 00:25:27 +0300

    update create

--- console.py
+++ console.py
@@ -131,8 +131,9 @@ class HBNBCommand(cmd.Cmd):
                         elif isinstance(param[1], str):
                             param[1] = param[1].replace("\\", "")
                             param[1] = param[1].replace("'", '')
+                            print(param[1])
                             param[1] = param[1][1: -1]
-                            print(param[0])
+                            print(param[1])
                         s.__dict__[param[0]] = param[1]
                         storage.save()
             print(s.id)
