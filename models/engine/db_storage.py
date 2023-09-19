d0fdd111c6cac4686b96fcb6dc2279b5cb5df156
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-20 02:58:00 +0300

    DBStorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -29,7 +29,7 @@ class DBStorage:
 		password = os.getenv('HBNB_MYSQL_PWD')
 		host = os.getenv('HBNB_MYSQL_HOST')
 		database = os.getenv('HBNB_MYSQL_DB')
-		self.__engine = create_engine("mysql+mysqldb://{}:l{}@{}/{}"
+		self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}"
 								.format(user, password, host, database), pool_pre_ping=True)
 		if os.getenv('HBNB_ENV') == 'test':
 			Base.metadata.drop_all(bind=self.__engine)
