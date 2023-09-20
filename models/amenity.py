2a3408adf6acbb20429c71f8f32a78ff88dfb373
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-20 17:19:18 +0300

    DBStorage

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
-import os
+"""
+Module with class Amenity
+"""
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModel, Base
-
 
 class Amenity(BaseModel, Base):
-    """Represents an amenity data set."""
-    __tablename__ = 'amenities'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    """
+    Amenity
+    """
+    name = Column(String(128), nullable=False)
+    __tablename__ = "amenities"
