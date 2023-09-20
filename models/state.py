ad8656c8ecda2b425d654ae4e4d90b650daa02ab
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-20 15:44:47 +0300

    DBStorage

--- models/state.py
+++ models/state.py
@@ -5,6 +5,7 @@ Module with class State
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models.city import City
 
 
 class State(BaseModel, Base):
