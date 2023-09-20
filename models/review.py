dc404bfa1d6e837f6164ae5b2168e4e5709990ba
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-20 18:36:12 +0300

    DBStorage

--- models/review.py
+++ models/review.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""
-Module with class Review
-"""
+""" Review module for the HBNB project """
+import os
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
+
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel, Base):
-    """
-    Review
-    """
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    text = Column(String(1024), nullable=False)
-    __tablename__ = "reviews"
+    """ Review classto store review information """
+    __tablename__ = 'reviews'
+    place_id = Column(
+        String(60), ForeignKey('places.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    user_id = Column(
+        String(60), ForeignKey('users.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    text = Column(
+        String(1024), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
