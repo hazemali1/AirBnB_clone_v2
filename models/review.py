fa8aa0b7829141edfa24d335bc455bfaef95556e
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-20 03:54:26 +0300

    DBStorage

--- models/review.py
+++ models/review.py
@@ -10,7 +10,7 @@ class Review(BaseModel, Base):
     """
     Review
     """
-    place_id = Column(String(60), ForeignKey('places_id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users_id'), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
     __tablename__ = "reviews"
