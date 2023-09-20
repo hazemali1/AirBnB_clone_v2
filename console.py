09986a68df979e7c656e22d42aea1f2c1cc5044b
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-20 22:31:57 +0300

    DBStorage

--- console.py
+++ console.py
@@ -10,6 +10,13 @@ from models.engine.file_storage import class_dict
 from datetime import datetime
 import os
 import uuid
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -17,6 +24,12 @@ class HBNBCommand(cmd.Cmd):
     HBNB comands
     """
     prompt = "(hbnb) "
+    
+    classes = {
+               'BaseModel': BaseModel, 'User': User, 'Place': Place,
+               'State': State, 'City': City, 'Amenity': Amenity,
+               'Review': Review
+               }
 
     def default(self, arg):
         """Default
