ecbe097ad4fda76b55317f016e4338a3fb08b109
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-20 17:55:32 +0300

    DBStorage

--- models/base_model.py
+++ models/base_model.py
@@ -22,25 +22,28 @@ class BaseModel:
     updated_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
     
     def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ('created_at', 'updated_at'):
-                        setattr(self, key, datetime.fromisoformat(value))
-                    else:
-                        setattr(self, key, value)
-            # if os.getenv('HBNB_TYPE_STORAGE') in ('db'):
+        """
+        Instantiation
+        """
+        date_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "__class__":
+                    continue
+                if k == "updated_at" or k == "created_at":
+                    setattr(self, k, datetime.fromisoformat(v))
+                else:
+                    setattr(self, k, v)
             if not hasattr(kwargs, 'id'):
                 setattr(self, 'id', str(uuid.uuid4()))
             if not hasattr(kwargs, 'created_at'):
                 setattr(self, 'created_at', datetime.now())
             if not hasattr(kwargs, 'updated_at'):
                 setattr(self, 'updated_at', datetime.now())
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
