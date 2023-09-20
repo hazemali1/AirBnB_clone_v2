a5b57febf8763cee80ae07cf792d083ac123c378
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-20 17:57:10 +0300

    DBStorage

--- models/base_model.py
+++ models/base_model.py
@@ -36,8 +36,6 @@ class BaseModel:
                     setattr(self, k, v)
             if not hasattr(kwargs, 'id'):
                 setattr(self, 'id', str(uuid.uuid4()))
-            if not hasattr(kwargs, 'created_at'):
-                setattr(self, 'created_at', datetime.now())
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
