9bc8b742f59c291443ade4a9db3f45569eb9ed5c
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-05 19:47:24 +0300

    sets up

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,5 +11,5 @@ echo -e '<html>\n <head>\n </head>\n <body>\n  Hello School\n </body>\n</html>' 
 rm -fr /data/web_static/current
 ln -s /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data/
-sed -i '^/server {/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sed -i '/^server {/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 service nginx restart
