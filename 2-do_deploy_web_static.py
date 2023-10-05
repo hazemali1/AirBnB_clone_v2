f53f84bca5521540e9fcade7dd81fed12845fc06
hazemali1 <hazemali1343@gmail.com>
2023-10-05 15:55:39 -0700

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,12 +24,13 @@ def do_deploy(archive_path):
                 break
             s += i
         run("mkdir -p /data/web_static/releases/{}".format(s))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(archive_pat, s))
+        d = "/data/web_static/releases/"
+        run("tar -xzf /tmp/{} -C {}{}/".format(archive_pat, d, s))
         run("rm /tmp/{}".format(archive_pat))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(s, s))
+        run("mv {}{}/web_static/* {}{}/".format(d, s, d, s))
         run("rm -rf /data/web_static/releases/{}/web_static".format(s))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(s))
+        run("ln -s {}{}/ /data/web_static/current".format(d, s))
         return True
     except SyntaxError:
         return False
