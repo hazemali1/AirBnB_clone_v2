afc77c03f8a006c466cf8f9c4881112824580d8e
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-05 19:24:36 +0300

    sets up

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,26 +2,14 @@
 # sets up wep server
 apt-get update
 apt-get install -y nginx
-if ! test -f /data/; then
-	mkdir /data/
-fi
-if ! test -f /data/web_static/; then
-	mkdir /data/web_static/
-fi
-if ! test -f /data/web_static/releases/; then
-	mkdir /data/web_static/releases/
-fi
-if ! test -f /data/web_static/shared/; then
-	mkdir /data/web_static/shared/
-fi
-if ! test -f /data/web_static/releases/test/; then
-	mkdir /data/web_static/releases/test/
-fi
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
 echo -e '<html>\n <head>\n </head>\n <body>\n  Hello School\n </body>\n</html>' > /data/web_static/releases/test/index.html
-if test -f /data/web_static/current; then
-	rm -r /data/web_static/current
-fi
+rm -fr /data/web_static/current
 ln -s /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+chown -hR ubuntu:ubuntu /data/
 sed -i '/server {/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 service nginx restart
