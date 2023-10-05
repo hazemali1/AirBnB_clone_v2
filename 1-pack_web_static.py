3ca30748f457b1d99ec9d24aebe1c9e0edec7cb9
hazemali1 <hazemali1343@gmail.com>
2023-10-05 14:08:20 -0700

    pycode

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,15 +5,17 @@ import datetime
 fabric
 """
 
+
 def do_pack():
-	"""
-  do pack
-	"""
-	try:
-		t = datetime.datetime.now()
-		s = "web_static_" + str(t.year) + str(t.month) + str(t.day) + str(t.hour) + str(t.minute) + str(t.second) + ".tgz"
-		local("mkdir -p versions")
-		local("tar -cvzf versions/{}  web_static".format(s))
-		return "versions/{}".format(s)
-	except:
-		return None
+    """
+    do pack
+    """
+    try:
+        t = datetime.datetime.now()
+        s = "web_static_" + str(t.year) + str(t.month) + str(t.day)
+        s += str(t.hour) + str(t.minute) + str(t.second) + ".tgz"
+        local("mkdir -p versions")
+        local("tar -cvzf versions/{}  web_static".format(s))
+        return "versions/{}".format(s)
+    except SyntaxError:
+        return None
