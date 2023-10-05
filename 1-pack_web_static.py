b48b7beeb72878de51f54e57ad39c45b4360da5c
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-05 23:55:32 +0300

    do pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,14 +6,14 @@ fabric
 """
 
 def do_pack():
-        """
-        do pack
-				"""
-        try:
-                t = datetime.datetime.now()
-                s = "web_static_" + t.year + t.month + t.day + t.hour + t.minute + t.second + ".tgz"
-                local("mkdir -p versions")
-                local("tar -cvzf versions/{}  web_static".format(s))
-                return "versions/{}".format(s)
-        except:
-                return None
+	"""
+  do pack
+	"""
+	try:
+		t = datetime.datetime.now()
+		s = "web_static_" + str(t.year) + str(t.month) + str(t.day) + str(t.hour) + str(t.minute) + str(t.second) + ".tgz"
+		local("mkdir -p versions")
+		local("tar -cvzf versions/{}  web_static".format(s))
+		return "versions/{}".format(s)
+	except:
+		return None
