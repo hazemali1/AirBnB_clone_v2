ca7b35e2e12d2c0cbcdd6d580ceaa1f1024cbf52
hazemali1 <hazemali1343@gmail.com>
2023-10-05 15:51:45 -0700

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,6 +7,8 @@ fabric
 
 
 env.hosts = ['100.25.15.141', '100.25.19.252']
+
+
 def do_deploy(archive_path):
     """
     deploy
@@ -14,17 +16,17 @@ def do_deploy(archive_path):
     if os.path.exists(archive_path) is False:
         return False
     try:
-        archive_path = archive_path[9:]
-        put(archive_path, "/tmp/{}".format(archive_path))
-        run("mkdir -p /data/web_static/releases/{}".format(archive_path))
+        archive_pat = archive_path[9:]
+        put(archive_path, "/tmp/{}".format(archive_pat))
         s = ""
-        for i in archive_path:
+        for i in archive_pat:
             if i == '.':
                 break
             s += i
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(archive_path, s))
-        run("rm /tmp/{}".format(archive_path))
-        run("cp /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(s, s))
+        run("mkdir -p /data/web_static/releases/{}".format(s))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(archive_pat, s))
+        run("rm /tmp/{}".format(archive_pat))
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(s, s))
         run("rm -rf /data/web_static/releases/{}/web_static".format(s))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(s))
