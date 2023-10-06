8254b4e010a1b3f7c31ddf70aec280b3af379315
hazemali1 <hazemali1343@gmail.com>
2023-10-05 19:17:43 -0700

    clean

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -17,24 +17,27 @@ def do_clean(number=0):
     if number == 0:
         number = 1
     num = 0
-    l = []
+    li = []
     for i in os.listdir("versions/"):
         num += 1
         q = i[11:-4]
-        l.append(q)
-    l.sort()
+        li.append(q)
+    li.sort()
     for x in range(num - int(number)):
-        local("rm versions/web_static_{}.tgz".format(l[x]))
+        local("rm versions/web_static_{}.tgz".format(li[x]))
 
-    num = 0
-    l = []
+    n = 0
+    lis = []
     local("mkdir -p help")
-    get(remote_path="/data/web_static/releases/*", local_path="help/")
+    try:
+        get(remote_path="/data/web_static/releases/web_static_*.tgz", local_path="help/")
+    except:
+        pass
     for i in os.listdir("help/"):
-        num += 1
-        q = i[11:-4]
-        l.append(q)
-    l.sort()
+        n += 1
+        qi = i[11:-4]
+        lis.append(qi)
+    lis.sort()
     for x in range(num - int(number)):
-        run("rm /data/web_static/releases/web_static_{}.tgz".format(l[x]))
+        run("rm /data/web_static/releases/web_static_{}.tgz".format(lis[x]))
     local("rm -rf help")
