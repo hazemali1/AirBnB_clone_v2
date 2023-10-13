49b9970bc9d56ee95dd59e58fa66cee1b0f55d0d
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-13 22:39:59 +0300

    clean

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -16,28 +16,16 @@ def do_clean(number=0):
     """
     if number == 0 or number == 1:
         number = 2
-    num = 0
-    li = []
-    for i in os.listdir("versions/"):
-        num += 1
-        q = i[11:-4]
-        li.append(q)
-    li.sort()
-    for x in range(num - int(number)):
-        local("rm versions/web_static_{}.tgz".format(li[x]))
+    with lcd('versions'):
+        archives = sorted(os.listdir('.'))
+        to_delete = archives[:-number]
 
-    n = 0
-    lis = []
-    local("mkdir -p help")
-    try:
-        get(remote_path="/data/web_static/releases/web_static_*.tgz", local_path="help/")
-    except:
-        pass
-    for i in os.listdir("help/"):
-        n += 1
-        qi = i[11:-4]
-        lis.append(qi)
-    lis.sort()
-    for x in range(num - int(number)):
-        run("rm /data/web_static/releases/web_static_{}.tgz".format(lis[x]))
-    local("rm -rf help")
+        for archive in to_delete:
+            local('rm -f {}'.format(archive))
+
+    with cd('/data/web_static/releases'):
+        archives = run('ls -tr').split()
+        to_delete = archives[:-number]
+
+        for archive in to_delete:
+            run('rm -f {}'.format(archive))
