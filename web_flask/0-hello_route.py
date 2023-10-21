5144e95482641ff071f21eed02f0042f4165b5dd
hazemali1 <hazemali1343@gmail.com>
2023-10-21 02:49:59 -0700

    pycode

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,14 +10,14 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
-	"""
-	hbnb
-	"""
-	return "Hello HBNB!"
+    """
+    hbnb
+    """
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
-	"""
-	main
-	"""
-	app.run(host='0.0.0.0', port=5000)
+    """
+    main
+    """
+    app.run(host='0.0.0.0', port=5000)
