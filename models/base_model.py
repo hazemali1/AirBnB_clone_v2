ce29ffef02a82ccf2fa62e3a5cb7879b46fe3986
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-22 15:10:46 +0300

    close

--- models/base_model.py
+++ models/base_model.py
@@ -37,6 +37,7 @@ class BaseModel:
             if not hasattr(kwargs, 'id'):
                 setattr(self, 'id', str(uuid.uuid4()))
             self.created_at = datetime.now()
+            self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
