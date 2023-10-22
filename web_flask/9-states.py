4d87c8f8fb59de5a7229aec579dee711ceee9237
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-23 00:09:25 +0300

    states

---
+++ web_flask/9-states.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+import
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+"""
+flask
+"""
+
+
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """
+    hbnb
+    """
+    states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
+    return render_template("9-states.html", states=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def cities_by_state_id(id):
+    """
+    hbnb
+    """
+    for i in storage.all(State).values():
+        if i.id == id:
+            state_id = i
+    return render_template("9-states.html", state_id=id)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """
+    teardown app context
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    """
+    main
+    """
+    app.run(host='0.0.0.0', port=5000)
