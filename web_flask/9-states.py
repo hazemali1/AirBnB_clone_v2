d43aa427828772da791e0674b71179d04e13603d
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-23 00:34:36 +0300

    states

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -35,12 +35,7 @@ def cities_by_state_id(id):
     return render_template("9-states.html", state_id=state_id)
 
 
-@app.teardown_appcontext
-def teardown(exception):
-    """
-    teardown app context
-    """
-    storage.close()
+
 
 
 if __name__ == '__main__':
