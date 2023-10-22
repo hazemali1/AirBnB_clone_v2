eb63a6ec919a73d770eb32b02d3118f297b239a0
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-22 14:45:58 +0300

    close

--- models/state.py
+++ models/state.py
@@ -1,35 +1,31 @@
 #!/usr/bin/python3
-"""This is the state class"""
-import os
-import models
+"""
+Module with class State
+"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models.city import City
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-        cities = relationship between state and city tables.
-    """
-
-    __tablename__ = 'states'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship(
-            'City', back_populates='state',
-            cascade='all, delete, delete-orphan')
-
-    else:
-        name = ""
-
-        @property
-        def cities(self):
-            """returns list of Cities and some relationships"""
-            cities_instances = []
-            cities_dict = models.storage.all(models.City)
-            for key, value in cities_dict.items():
-                if self.id == value.state_id:
-                    cities_instances.append(value)
-            return cities_instances
+	"""
+  State
+  """
+	__tablename__ = "states"
+	if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+		cities = relationship("City", backref="State", cascade="all, delete")
+		name = Column(String(128), nullable=False)
+	else:
+		name = ""
+		def cities(self):
+			"""
+      list of city
+      """
+			from models import storage
+			l = []
+			for k, v in storage.all(City).items():
+				if v.state_id == self.id:
+					l.append(v)
+			return l
+        
