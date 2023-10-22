591c7bee5c70cc22c1d9573f51e4de85783cb04e
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-22 14:47:45 +0300

    close

--- models/state.py
+++ models/state.py
@@ -6,6 +6,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.city import City
+import os
 
 
 class State(BaseModel, Base):
